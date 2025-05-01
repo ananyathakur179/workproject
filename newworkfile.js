cdbf81c77dc4a61470d241165868712e498fd148
Ananya80 <ananya.thakur@prismberry.com>
2025-05-01 12:43:53 +0530

    new commit

---
+++ newworkfile.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const newworkfile = () => {
+  return (
+    <View>
+      <Text>newworkfile</Text>
+    </View>
+  )
+}
+
+export default newworkfile
